c6426144e57d38b6ec38c44fef86b288969d3fe6
Sukanya <sukanyakumari19@gmail.com>
2025-06-10 18:54:15 +0530

    update

--- playwright.config.js
+++ playwright.config.js
@@ -31,6 +31,8 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+    video: 'on', // Options: 'off', 'on', 'retain-on-failure', 'on-first-retry'
+
   },
 
   /* Configure projects for major browsers */
@@ -51,10 +53,10 @@ export default defineConfig({
     },*/
 
     /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: { ...devices['Pixel 5'] },
-    // },
+     {
+       name: 'Mobile Chrome',
+       use: { ...devices['Pixel 6'] },
+     },
     // {
     //   name: 'Mobile Safari',
     //   use: { ...devices['iPhone 12'] },
